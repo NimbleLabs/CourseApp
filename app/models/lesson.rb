14885acab97967a6f77c8946e932d260c06a05e3
Harris Reynolds <harris@nimblelabs.com>
2018-11-20 16:42:39 -0600

    support for availability options

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,9 +1,25 @@
 class Lesson < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: [:slugged, :finders]
-
   belongs_to :unit
-
   has_one_attached :image
   has_one_attached :video
+
+  FREE = 'free'
+  SIGNUP = 'signup'
+  PRO = 'pro'
+  AVAILABILITY_OPTIONS = [Lesson::FREE, Lesson::SIGNUP, Lesson::PRO]
+
+  def free?
+    self.availability == Lesson::FREE
+  end
+
+  def signed_up?
+    self.availability == Lesson::SIGNUP
+  end
+
+  def pro?
+    self.availability == Lesson::PRO
+  end
+
 end
