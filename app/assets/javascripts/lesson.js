dff17298b31c21deea4a595ac9c8d98375973950
Harris Reynolds <harris@nimblelabs.com>
2018-12-12 14:20:03 -0600

    Remove ES6 syntax

--- app/assets/javascripts/lesson.js
+++ app/assets/javascripts/lesson.js
@@ -1,21 +1,21 @@
 var videoElements = document.getElementsByTagName("video");
 
 if(videoElements && videoElements[0]) {
-    console.log('Setting up video events')
+    
     var v = videoElements[0]
-    v.addEventListener("seeked", (event) => {
+    v.addEventListener("seeked", function(event) {
         ahoy.track("$seeked", {lesson_id: NIMBLE_HQ_LESSON_ID, page: window.location.pathname});
     }, true);
 
-    v.addEventListener("ended", (event) => {
+    v.addEventListener("ended", function(event) {
         ahoy.track("$ended", {lesson_id: NIMBLE_HQ_LESSON_ID, page: window.location.pathname});
     }, true);
 
-    v.addEventListener("play", (event) => {
+    v.addEventListener("play", function(event) {
         ahoy.track("$play", {lesson_id: NIMBLE_HQ_LESSON_ID, page: window.location.pathname});
     }, true);
 
-    v.addEventListener("pause", (event) => {
+    v.addEventListener("pause", function(event) {
         ahoy.track("$pause", {lesson_id: NIMBLE_HQ_LESSON_ID, page: window.location.pathname});
     }, true);
 }
