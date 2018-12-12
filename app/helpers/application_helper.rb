61f116158d7191cb9785942038611b872d428b48
Harris Reynolds <harris@nimblelabs.com>
2018-12-11 18:00:15 -0600

    Back fill home page and signup improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def devise_error_messages
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+
+    html = <<-HTML
+    <div id="error_explanation" class="alert alert-warning">
+      <h4 class="alert-heading">Registration error(s):</h4>
+      <ul>#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :path_names => {:sign_in => 'signin', :sign_up => 'register', :sign_out => 'logout'},
              :controllers => {registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks'}
 
+  get 'privacy', to: 'home#privacy', as: 'privacy'
+  get 'terms', to: 'home#terms', as: 'terms'
   get 'home/index'
   root to: 'home#index'
 end
