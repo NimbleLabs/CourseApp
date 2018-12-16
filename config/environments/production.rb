8dce9ea73b62a1ad7bf10425ccb7648852811585
Harris Reynolds <harris@nimblelabs.com>
2018-12-16 07:15:01 -0600

    Fix for email links

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,6 +100,8 @@ Rails.application.configure do
       :enable_starttls_auto => true
   }
 
+  config.action_mailer.default_url_options = { host: 'www.nimblehq.com', :protocol => 'https' }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
