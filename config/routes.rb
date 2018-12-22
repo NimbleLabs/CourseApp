f658cd974f4c3d83e30e6c45f56da1bc36c5d111
Harris Reynolds <harris@nimblelabs.com>
2018-12-22 09:12:29 -0600

    Implement a simple account page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'account', to: 'account#index', as: 'account'
   devise_for :users, :path_names => {:sign_in => 'signin', :sign_up => 'register', :sign_out => 'logout'},
              :controllers => {registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks'}
 
