5e309d76e4c5591cbdf7ef8b1374864ec2b0f980
Harris Reynolds <harris@nimblelabs.com>
2019-01-15 12:27:10 -0600

    Add very simple admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   layout :layout_by_resource
+
+  def after_sign_in_path_for(resource)
+    if session[:custom_redirect_url].present?
+      custom_redirect_url = session[:custom_redirect_url]
+      session[:custom_redirect_url] = nil
+      return custom_redirect_url
+    end
+
+    root_path
+  end
 
   private
 
@@ -9,6 +21,19 @@ class ApplicationController < ActionController::Base
       'devise'
     else
       'application'
+    end
+  end
+
+  def ensure_admin
+    return true if user_signed_in? && current_user.admin?
+
+    redirect_to root_path, flash: {alert: "You don't have enough permissions to proceed"}
+    return false
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation, :full_name)
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :path_names => {:sign_in => 'signin', :sign_up => 'register', :sign_out => 'logout'},
              :controllers => {registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks'}
 
+  get 'admin', to: 'admin#index', as: 'admin'
   get 'privacy', to: 'home#privacy', as: 'privacy'
   get 'terms', to: 'home#terms', as: 'terms'
   get 'home/index'
